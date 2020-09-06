acc07218403e8f9e1d99d6db0454a46903a45a0b
mh <hamptonio@gmail.com>
2020-09-06 15:50:38 -0500

    Added Ornstein-Uhlenbeck process

---
+++ src/OrnsteinUhlenbeck.cpp
@@ -0,0 +1,96 @@
+/*
+ * Copyright (c) 2020 Marshall Hampton <contact hamptonio at gmail.com>
+ */
+#include "plugin.hpp"
+#include <array>
+
+struct OrnsteinUhlenbeck : Module {
+
+	enum ParamIds {
+		NOISE_PARAM,
+		SPRING_PARAM,
+		MEAN_PARAM,
+		NUM_PARAMS
+	};
+	enum InputIds {
+		NOISE_INPUT,
+		SPRING_INPUT,
+		MEAN_INPUT,
+		TRIG_INPUT,
+		NUM_INPUTS
+	};
+	enum OutputIds {
+		SIG_OUTPUT,
+		NUM_OUTPUTS
+	};
+
+	float outsignal[16] = {0};
+	std::array<rack::dsp::SchmittTrigger,16> inputTrigger;
+	float sqrtdelta = 1.0/std::sqrt(APP->engine->getSampleRate());
+
+	OrnsteinUhlenbeck() {
+		config(NUM_PARAMS, NUM_INPUTS, NUM_OUTPUTS);
+		configParam(NOISE_PARAM, 0.f, 1.f, 0.f, "Noise level");
+		configParam(SPRING_PARAM, 0.f, 1.f, 0.f, "Mean reverting strength");
+		configParam(MEAN_PARAM, -10.f, 10.f, 1.f, "Mean","", 2.0);
+	}
+
+	void onSampleRateChange() override {
+		sqrtdelta = 1.0/std::sqrt(APP->engine->getSampleRate());
+	}
+
+	void process(const ProcessArgs& args) override {
+
+		int channels = std::max(inputs[TRIG_INPUT].getChannels(),
+			std::max(inputs[NOISE_INPUT].getChannels(),
+			std::max(inputs[SPRING_INPUT].getChannels(),
+			std::max(inputs[MEAN_INPUT].getChannels(),
+			1))));
+
+		for (int c = 0; c < channels; c++) {
+			float noise = params[NOISE_PARAM].getValue() + inputs[NOISE_INPUT].getVoltage(c)/10.0f;
+			float spring = params[SPRING_PARAM].getValue() + inputs[SPRING_INPUT].getVoltage(c);
+			float mean = params[MEAN_PARAM].getValue() + inputs[MEAN_INPUT].getVoltage(c);
+		
+			if (inputTrigger[c].process(inputs[TRIG_INPUT].getVoltage(c))){
+				outsignal[c] = mean;
+			}
+
+			float r = random::normal(); 
+			outsignal[c] += sqrtdelta*r*noise;
+			outsignal[c] += spring*(mean-outsignal[c])*args.sampleTime;
+
+		}
+		outputs[SIG_OUTPUT].setChannels(channels);
+        
+	}
+};
+
+
+
+struct OrnsteinUhlenbeckWidget : ModuleWidget {
+	OrnsteinUhlenbeckWidget(OrnsteinUhlenbeck* module) {
+		setModule(module);
+		setPanel(APP->window->loadSvg(asset::plugin(pluginInstance, "res/OrnsteinUhlenbeck.svg")));
+
+		addChild(createWidget<ScrewSilver>(Vec(RACK_GRID_WIDTH, 0)));
+		addChild(createWidget<ScrewSilver>(Vec(box.size.x - 2 * RACK_GRID_WIDTH, 0)));
+		addChild(createWidget<ScrewSilver>(Vec(RACK_GRID_WIDTH, RACK_GRID_HEIGHT - RACK_GRID_WIDTH)));
+		addChild(createWidget<ScrewSilver>(Vec(box.size.x - 2 * RACK_GRID_WIDTH, RACK_GRID_HEIGHT - RACK_GRID_WIDTH)));
+
+		addParam(createParamCentered<RoundBlackKnob>(mm2px(Vec(20.0, 26.0)), module, OrnsteinUhlenbeck::NOISE_PARAM));
+		addParam(createParamCentered<RoundBlackKnob>(mm2px(Vec(20.0, 58.0)), module, OrnsteinUhlenbeck::MEAN_PARAM));
+
+		addInput(createInputCentered<PJ301MPort>(mm2px(Vec(8, 39)), module, OrnsteinUhlenbeck::NOISE_INPUT));
+		addInput(createInputCentered<PJ301MPort>(mm2px(Vec(8, 69)), module, OrnsteinUhlenbeck::MEAN_INPUT));
+		addInput(createInputCentered<PJ301MPort>(mm2px(Vec(8, 86)), module, OrnsteinUhlenbeck::TRIG_INPUT));
+
+		addOutput(createOutputCentered<PJ301MPort>(mm2px(Vec(24, 109)), module, OrnsteinUhlenbeck::SIG_OUTPUT));
+
+
+
+	}
+};
+
+
+Model* modelOrnsteinUhlenbeck = createModel<OrnsteinUhlenbeck, OrnsteinUhlenbeckWidget>("OrnsteinUhlenbeck");
--- src/plugin.hpp
+++ src/plugin.hpp
@@ -9,3 +9,4 @@ extern Plugin* pluginInstance;
 
 // Declare each Model, defined in each module source file
 extern Model* modelBrownianBridge;
+extern Model* modelOrnsteinUhlenbeck;
